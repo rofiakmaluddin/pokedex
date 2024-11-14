544c95074a5a9b160881a218b2fdb797a14c937e
rofiakmaluddin <r.akmaluddin@gmail.com>
2024-11-15 06:51:27 +0700

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,40 @@
+import type { Metadata } from 'next';
+import localFont from 'next/font/local';
+import './globals.css';
+import QueryProvider from '@/providers/react-query';
+
+const geistSans = localFont({
+  src: './fonts/GeistVF.woff',
+  variable: '--font-geist-sans',
+  weight: '100 900',
+});
+const geistMono = localFont({
+  src: './fonts/GeistMonoVF.woff',
+  variable: '--font-geist-mono',
+  weight: '100 900',
+});
+
+export const metadata: Metadata = {
+  title: 'Pokedex',
+  description: 'Find your favourite Pokemon',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <QueryProvider>
+          <main className="container mx-auto bg-neutral-100 text-app-text min-h-screen">
+            {children}
+          </main>
+        </QueryProvider>
+      </body>
+    </html>
+  );
+}
---
+++ lib/react-query-client.ts
@@ -0,0 +1,12 @@
+import { QueryClient } from '@tanstack/react-query';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 2,
+      refetchOnWindowFocus: true,
+    },
+  },
+});
+
+export default queryClient;
